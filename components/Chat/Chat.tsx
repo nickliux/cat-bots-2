32c33ed019c30581e63ac295320d0d5ad53fc31a
nickliux <nick.liu.engineering@gmail.com>
2023-05-16 22:09:54 -0400

    Fix comment bug

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -408,9 +408,10 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                       'Say Hi!'
                     )}
                   </div>
-
+                  
+                  {
                   {/*
-                  {models.length > 0 && (
+                  models.length > 0 && (
                     <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
                       <ModelSelect />
 
@@ -435,7 +436,9 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                           })
                         }
                       />
-                      */}
+                    
+                    */}
+                    }
 
                     </div>
                   )}
